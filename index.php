1af7343b85b61207aaf4a8e093777816b039e135
EtskaCoding <etskacoding@gmail.com>
2021-04-11 22:34:09 +0300

    Added Recaptcha v3

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 session_start();
+define('sitekey', 'YOUR SITE KEY');
 ?>
+<!-- By: EtskaCoding https://github.com/EtskaCoding -->
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>BugReport</title>
         <link rel="stylesheet" href="style.css"/>
+        <script
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
+
+        <script src="https://www.google.com/recaptcha/api.js?render=<?php echo sitekey; ?>"></script>
     </head>
     <body>
         <?php
@@ -15,11 +23,28 @@ session_start();
             unset($_SESSION['msg']);
         }
         ?>
-        <form name="bugreport" action="send.php" method="POST" enctype="multipart/form-data">
+        <form name="bugreport" id="bugreport" action="send.php" method="POST" enctype="multipart/form-data">
             <input type="text" placeholder="Your player name" name="playername" id="playername">
             <input type="text" placeholder="Your discord name or email" name="dcname" id="dcname">
             <textarea id="description" name="description" rows="5" cols="10" style="resize: none;"></textarea>
             <input type="submit" value="Report!">
         </form>
     </body>
+    <script>
+        $('#bugreport').submit(function (event) {
+            event.preventDefault();
+            var playername = $('#playername').val();
+            var dcname = $('#dcname').val();
+            var description = $('#description').val();
+
+            grecaptcha.ready(function () {
+                grecaptcha.execute('<?php echo sitekey; ?>', {action: 'bugreport'}).then(function (token) {
+                    $('#bugreport').prepend('<input type="hidden" name="token" value="' + token + '">');
+                    $('#bugreport').prepend('<input type="hidden" name="action" value="bugreport">');
+                    $('#bugreport').unbind('submit').submit();
+                });
+                ;
+            });
+        });
+    </script>
 </html>
--- send.php
+++ send.php
@@ -1,35 +1,117 @@
 <?php
-$webhookurl = "YOUR_WEBHOOK_URL";//PASTE YOUR WEBHOOK URL HERE!
+
+// By: EtskaCoding https://github.com/EtskaCoding
+
+$webhookurl = "YOUR WEBHOOK URL"; //PASTE YOUR WEBHOOK URL HERE!
 
 session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $playername = $_POST['playername'];
-    $dcname = $_POST['dcname'];
-    $description = $_POST['description'];
-    
-    if (empty($playername)) {
-        $_SESSION['msg'] = '<div class="alert">
+
+define("RECAPTCHA_V3_SECRET_KEY", 'YOUR SECRET KEY');
+  
+$token = $_POST['token'];
+$action = $_POST['action'];
+  
+// call curl to POST request
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_V3_SECRET_KEY, 'response' => $token)));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+$arrResponse = json_decode($response, true);
+  
+// verify the response
+if($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.5) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $playername = $_POST['playername'];
+        $dcname = $_POST['dcname'];
+        $description = $_POST['description'];
+
+        if (empty($playername)) {
+            $_SESSION['msg'] = '<div class="alert">
             Player name is required!
             </div>';
-    }
-    
-    
-    elseif (empty($dcname)) {
-        $_SESSION['msg'] = '<div class="alert">
+        } elseif (empty($dcname)) {
+            $_SESSION['msg'] = '<div class="alert">
             Discord name or email is required!
             </div>';
-    }
-    
-    
-    elseif (empty($description)) {
-        $_SESSION['msg'] = '<div class="alert">
+        } elseif (empty($description)) {
+            $_SESSION['msg'] = '<div class="alert">
             Description is required!
             </div>';
+        } else {
+            send($playername, $dcname, $description);
+            $_SESSION['msg'] = '<div class="success">
+            Bug report sended succesfully! Staff contacts you soon.
+            </div>';
+        }
+
+
+        header("Location: index.php");
+        exit();
     } else {
-        
-    }
+    // spam submission
+    // show error message
+}
 
+}
 
-    header("Location: index.php");
-    exit();
+function send($playername, $dcname, $description) {
+    global $webhookurl;
+    $json_data = json_encode([
+        // Username
+        "username" => "BugReport",
+        // Avatar URL.
+        // Uncoment to replace image set in webhook
+        "avatar_url" => "https://i.redd.it/b1b640z9b3c21.jpg",
+        // Embeds Array
+        "embeds" => [
+            [
+                // Embed Title
+                "title" => "BugReport",
+                // Embed Type
+                "type" => "rich",
+                // Embed left border color in HEX
+                "color" => hexdec("3366ff"),
+                // Footer
+                "footer" => [
+                    "text" => "By EtskaCoding",
+                    "icon_url" => "https://etska.ml/etska.png"
+                ],
+                // Additional Fields array
+                "fields" => [
+                    [
+                        "name" => "Player Name",
+                        "value" => $playername,
+                        "inline" => false
+                    ],
+                    [
+                        "name" => "Discord or email",
+                        "value" => $dcname,
+                        "inline" => false
+                    ],
+                    [
+                        "name" => "Description",
+                        "value" => $description,
+                        "inline" => false
+                    ]
+                // Etc..
+                ]
+            ]
+        ]
+            ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+    $ch = curl_init($webhookurl);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $response = curl_exec($ch);
+// If you need to debug, or find out why you can't send message uncomment line below, and execute script.
+// echo $response;
+    curl_close($ch);
 }
