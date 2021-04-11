4a312b07b9849532fe4258318fcbd65d014f183f
EtskaCoding <etskacoding@gmail.com>
2021-04-11 21:59:37 +0300

    Updated

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ session_start();
         <form name="bugreport" action="send.php" method="POST" enctype="multipart/form-data">
             <input type="text" placeholder="Your player name" name="playername" id="playername">
             <input type="text" placeholder="Your discord name or email" name="dcname" id="dcname">
-            <textarea id="description" name="name" rows="5" cols="10" style="resize: none;"></textarea>
+            <textarea id="description" name="description" rows="5" cols="10" style="resize: none;"></textarea>
             <input type="submit" value="Report!">
         </form>
     </body>
--- send.php
+++ send.php
@@ -1,13 +1,35 @@
 <?php
+$webhookurl = "YOUR_WEBHOOK_URL";//PASTE YOUR WEBHOOK URL HERE!
 
 session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $playername = $_POST['playername'];
+    $dcname = $_POST['dcname'];
+    $description = $_POST['description'];
+    
     if (empty($playername)) {
+        $_SESSION['msg'] = '<div class="alert">
+            Player name is required!
+            </div>';
+    }
+    
+    
+    elseif (empty($dcname)) {
+        $_SESSION['msg'] = '<div class="alert">
+            Discord name or email is required!
+            </div>';
+    }
+    
+    
+    elseif (empty($description)) {
+        $_SESSION['msg'] = '<div class="alert">
+            Description is required!
+            </div>';
+    } else {
         
     }
 
-    $_SESSION['msg'] = "Hello";
+
     header("Location: index.php");
     exit();
 }
